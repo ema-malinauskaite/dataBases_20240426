40465fa13aa21c5ea626734a378e02c9589f9496
ema-malinauskaite <ema.malinauskaite@gmail.com>
2024-04-29 08:36:09 +0300

    done

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-# reikejo terminale paleist kodus:
-# pip install mysql.connector
-# pip install mysql-connector-python[cext]
-
 import SQLCRUD as sql
-#------------------------------------------------------------------------------------------------------
-#atvaizduoti visus customerius
-print('---task 1---')
 
-results = sql.getSQLData('''
-    SELECT * FROM actor 
+#------------------------------------------------------------------------------------------------------
+print('  -- Customer names:')
+
+sakilaData = sql.getSQLData('''
+    SELECT * FROM sakila.actor 
 ''')
 
-for i in range(len(results)):
+for i in range(len(sakilaData)):
     if i < 5:
-        print(results[i][1:3])
+        print(sakilaData[i][1:3])
     if i == 5:
         print('Only first 5 rows printed.')
 
 #------------------------------------------------------------------------------------------------------
-# atvaizduoti visus customerius ir stulpelį kuriame būtų atvaizduota kiek pinigų kiekvienas jų yra išleidęs nuomai, ir kiek filmų nuomavesis
-print('---task 2---')
-results = getSQLData('''
+print('  -- Number of money spend and movies rented:')
+
+sakilaData = sql.getSQLData('''
     SELECT
         cust.first_name,
         cust.last_name,
         SUM(pay.amount),
-        COUNT(inv.film_id)'
+        COUNT(inv.film_id)
     FROM
         sakila.customer cust
             JOIN
@@ -37,26 +33,28 @@ results = getSQLData('''
     GROUP BY
     cust.first_name,
     cust.last_name
+    ORDER BY
+    SUM(pay.amount) DESC
 ''')
 
-for i in range(len(results)):
-    row = list(results[i])
+for i in range(len(sakilaData)):
+    row = list(sakilaData[i])
     row[2] = float(row[2])
     if i < 5:
-        print(results[i])
+        print(row)
     if i == 5:
         print('Only first 5 rows printed.')
+
 #------------------------------------------------------------------------------------------------------
-# atvaizduoti aktorius ir keliuose filmuose jie yra filmavesi
-print('---uzd3---')
-results = getSQLData('''
+print('  -- Number of movies actors played in:')
+
+sakilaData = sql.getSQLData('''
     SELECT 
-        actor.actor_id,
         actor.first_name,
         actor.last_name,
         COUNT(film.title) AS films_number
     FROM 
-        actor actor
+        sakila.actor
     JOIN 
         film_actor filmactor ON actor.actor_id = filmactor.actor_id
     JOIN 
@@ -64,86 +62,86 @@ results = getSQLData('''
     GROUP BY 
         actor.actor_id, actor.first_name, actor.last_name
     ORDER BY 
-        actor.actor_id
+        films_number DESC
 ''')
 
-for i in range(len(results)):
+for i in range(len(sakilaData)):
     if i < 5:
-        print(results[i])
+        print(sakilaData[i])
     if i == 5:
         print('Only first 5 rows printed.')
 
 #------------------------------------------------------------------------------------------------------
-# atvaizduoti visus filmus ir kiek aktorių juose vaidino
-print('---uzd4---')
-results = getSQLData('''
-SELECT 
-        film.film_id,
+print('  -- Number of actors that played in movies:')
+
+sakilaData = sql.getSQLData('''
+    SELECT 
         film.title,
-        COUNT(filmactor.actor_id) AS aktoriu_kiekis
+        COUNT(filmactor.actor_id) AS actor_number
     FROM 
-        film film
+        sakila.film
     LEFT JOIN 
         film_actor filmactor ON film.film_id = filmactor.film_id
     GROUP BY 
         film.film_id, film.title
     ORDER BY 
-        film.film_id
-        ''')
+        actor_number DESC
+''')
 
-for i in range(len(results)):
+for i in range(len(sakilaData)):
     if i < 5:
-        print(results[i])
+        print(sakilaData[i])
     if i == 5:
         print('Only first 5 rows printed.')
+
 #------------------------------------------------------------------------------------------------------
-# su pitono pagalba: nustatyti kuris nuomos punktas:
-#--turi daugiau customerių
-print('---uzd5---')
-results = getSQLData('''
-SELECT 
+print('  -- Number of customers  by store:')
+
+sakilaData = sql.getSQLData('''
+    SELECT 
         store.store_id,
-        COUNT(cust.customer_id) AS nuomininkai
+        COUNT(cust.customer_id) AS customers
     FROM 
-        store store
+        sakila.store
     JOIN 
         customer cust ON store.store_id = cust.store_id
     GROUP BY 
         store.store_id
     ORDER BY 
-        nuomininkai DESC
-        ''')
+        customers DESC
+''')
 
-for row in results:
+for row in sakilaData:
     print(row)
 
-#--išnuomavo daugiau(ir kiek kiekvienas) filmų
-print('---uzd6---')
-results = getSQLData('''
-  SELECT 
+#------------------------------------------------------------------------------------------------------
+print('   -- Number of movies rented by store:')
+
+sakilaData = sql.getSQLData('''
+    SELECT 
         store.store_id,
-        COUNT(invent.inventory_id) AS filmuSkaicius
+        COUNT(invent.inventory_id) AS films_number
     FROM 
-        store store
+        sakila.store
     JOIN 
         inventory invent ON store.store_id = invent.store_id
     GROUP BY 
         store.store_id
     ORDER BY 
-        filmuSkaicius DESC
-        ''')
+        films_number DESC
+''')
 
-for row in results:
+for row in sakilaData:
     print(row)
 
-#--kiek sugeneravo pajamų
-print('---uzd6---')
-results = getSQLData('''
- SELECT 
+
+print('  -- Profit by store:')
+sakilaData = sql.getSQLData('''
+    SELECT 
         store.store_id,
-        SUM(pay.amount) AS pelnas
+        SUM(pay.amount) AS profit
     FROM 
-        store store
+        sakila.store
     JOIN 
         staff staff ON store.manager_staff_id = staff.staff_id
     JOIN 
@@ -151,10 +149,10 @@ results = getSQLData('''
     GROUP BY 
         store.store_id
     ORDER BY 
-        pelnas DESC
-        ''')
+        profit DESC
+''')
 
-for row in results:
+for row in sakilaData:
     row = list(row)
     row[1] = float(row[1])
     print(row)
