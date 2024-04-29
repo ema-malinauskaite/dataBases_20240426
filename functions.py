3e70e3d8baf2194f9c446f107185eb8c0670a6e8
ema-malinauskaite <ema.malinauskaite@gmail.com>
2024-04-29 16:16:53 +0300

    added notes for functions

--- functions.py
+++ functions.py
@@ -5,12 +5,14 @@ username = "root"
 password = "pipiriukaS56"
 database = "sakila"
 portAddress = 3317
+
+
+# Imports data from sql server according to given SQL query as a list of tuples:
 def getSQLData(SQLquery):
     connection = None
     cursor = None
     try:
         connection = mysql.connector.connect(host=hostname, port=portAddress, user=username, password=password, database=database)
-
         cursor = connection.cursor()
         query = SQLquery
         cursor.execute(query)
@@ -25,12 +27,16 @@ def getSQLData(SQLquery):
         if connection:
             connection.close()
 
+
+# From given list of tupples, changes them to lists and number in indeex place to float:
 def changeToFloat(sampleData, index):
     for i in range(len(sampleData)):
         sampleData[i] = list(sampleData[i])
         sampleData[i][index] = float(sampleData[i][index])
     return sampleData
 
+
+# Prints given number of rows with a note at the end:
 def printRows(sampleData,lines = 0):
     for i in range(len(sampleData)):
         if lines == 0:
@@ -41,6 +47,8 @@ def printRows(sampleData,lines = 0):
             if i == lines:
                 print(f'Only first {lines} rows printed.')
 
+
+# From given list of tuples does GROUP BY sum (count = Falce and addValueFromIndex with value) or count by given value:
 def sumBy(sampleData, byValue, addValueFromIndex = None, count = True):
     outputDict = {}
     for i in sampleData:
